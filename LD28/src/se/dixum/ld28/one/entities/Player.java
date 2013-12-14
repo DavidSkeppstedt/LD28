8d2fc861f21c6989dcb0d812bd2875d6993a9a73
David Skeppstedt <david.skeppstedt@gmail.com>
2013-12-14 15:57:53 +0100

    Some kinde of collision detection

--- LD28/src/se/dixum/ld28/one/entities/Player.java
+++ LD28/src/se/dixum/ld28/one/entities/Player.java
@@ -8,16 +8,20 @@ import se.dixum.simple.utils.SimpleInput;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Shape;
 
 public class Player extends SimpleEntity implements SimpleBaseEntity {
 
 	SimpleAnimated sprite;
 	Animation right,left,up,down;
-	
+	ShapeRenderer s;
 	public Player() {
 	
 		init();
@@ -27,7 +31,7 @@ public class Player extends SimpleEntity implements SimpleBaseEntity {
 	
 	@Override
 	public void init() {
-		
+		s = new ShapeRenderer();
 		sprite = new SimpleAnimated(new Texture(Gdx.files.internal("gfx/player/player.png")),
 				32, 32, 0.24f);
 		right = sprite.createAnimation(0, 3, 0);
@@ -48,9 +52,10 @@ public class Player extends SimpleEntity implements SimpleBaseEntity {
 		movement();
 		changePosition(delta);
 		
-		
-		
-		
+		if (Gdx.input.isKeyPressed(Keys.R)) {
+			sprite.setPosition(new Vector2(320,100));
+		}
+	
 		
 	}
 	
@@ -64,21 +69,23 @@ public class Player extends SimpleEntity implements SimpleBaseEntity {
 	private void movement() {
 		
 		
-		if (checkCollision(SimpleInput.RIGHT) && !(SimpleInput.UP ||SimpleInput.DOWN)) {
+		if (checkCollision(SimpleInput.RIGHT,0) && !(SimpleInput.UP ||SimpleInput.DOWN)) {
 			sprite.setVelX(3);
 		}else {
 			sprite.setVelX(0);
 		}
-		if (checkCollision(SimpleInput.LEFT) && !(SimpleInput.UP||SimpleInput.DOWN)) {
+		if (checkCollision(SimpleInput.LEFT,1) && !(SimpleInput.UP||SimpleInput.DOWN)) {
 			sprite.setVelX(-3);
 		}
-		if (checkCollision(SimpleInput.UP) && !(SimpleInput.RIGHT||SimpleInput.LEFT)) {
+		if (checkCollision(SimpleInput.UP,2) && !(SimpleInput.RIGHT||SimpleInput.LEFT)) {
 			sprite.setVelY(3);
 		}else {
 			sprite.setVelY(0);
 		}
-		if (checkCollision(SimpleInput.DOWN)&& !(SimpleInput.RIGHT||SimpleInput.LEFT)) {
+		if (checkCollision(SimpleInput.DOWN,3)&& !(SimpleInput.RIGHT||SimpleInput.LEFT)) {
 			sprite.setVelY(-3);
+		}else if (!SimpleInput.UP){
+			sprite.setVelY(0);
 		}
 		
 		
@@ -87,19 +94,50 @@ public class Player extends SimpleEntity implements SimpleBaseEntity {
 	}
 
 
-	private boolean checkCollision(boolean key) {
+	private boolean checkCollision(boolean key,int i) {
 		if (key){
 			
 			// 2 == collision
-			float  x= (sprite.getPosition().x + sprite.getFrameWidth()*2/2)/32;
-			float  y = (sprite.getPosition().y + sprite.getFrameHeight()*2/2)/32;
+			float  x= (sprite.getPosition().x + 32)/32;
+			float  y = (sprite.getPosition().y + 32)/32;
 			
-			
-			if (GameScreen.home.getTile(x, y) != 2) {
-				return true;
-			}else {
-				return false;
+			switch(i) {
+			case 0:
+					
+				if (GameScreen.home.getTile(x+1,y) != 2 && GameScreen.home.getTile(x+1,y-1) != 2) {
+					return true;
+				}else {
+					return false;
+				}
+					
+					
+			case 1:
+				if (GameScreen.home.getTile(x-1,y) != 2 && GameScreen.home.getTile(x-1,y-1) != 2) {
+					return true;
+				}else {
+					return false;
+				}
+					
+	
+			case 2:
+				if (GameScreen.home.getTile(x, y+1) !=2) {
+					return true;
+				}else {
+					return false;
+				}	
+					
+			case 3:
+				if (GameScreen.home.getTile(x, y-1) !=2) {
+					
+					return true;
+				}else {
+					sprite.setPosition(sprite.getX(), sprite.getY()+4);
+					return false;
+				}	
+					
+					
 			}
+			
 			
 			
 			
@@ -134,6 +172,28 @@ public class Player extends SimpleEntity implements SimpleBaseEntity {
 	@Override
 	public void draw(SpriteBatch batch) {
 		sprite.drawAnimation(batch);
+		batch.end();
+		
+		s.begin(ShapeType.Line);
+			s.rect(sprite.getX(), sprite.getY(), 64, 64);
+			/*s.circle(sprite.getX()+32, sprite.getY()+32, 2);
+			
+			s.circle(sprite.getX()+64, sprite.getY()+32, 2);
+			s.circle(sprite.getX(), sprite.getY()+32, 2);
+			s.circle(sprite.getX()+64, sprite.getY(), 2);
+			s.circle(sprite.getX(), sprite.getY(), 2);
+			
+			
+			/*s.circle(sprite.getX(), sprite.getY(), 2);
+			/*s.circle(sprite.getX()+64, sprite.getY()+64, 2);
+			
+			
+			
+			/*s.circle(sprite.getX(), sprite.getY()+64, 2);
+			s.circle(sprite.getX()+64, sprite.getY(), 2);*/
+		s.end();
+		
+		batch.begin();
 
 	}
 
